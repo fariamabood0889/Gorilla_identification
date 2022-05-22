3210659a333c656a0f6480014dfe368a128c612c
Syeda Faria Mabood <62211563+fariamabood0889@users.noreply.github.com>
2022-05-22 15:31:27 +0530

    Update get_pretrain.sh

--- scripts/get_pretrain.sh
+++ scripts/get_pretrain.sh
@@ -1,5 +1,5 @@
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" -o yolor_p6.pt
+curl -c ./cookie -s -L "https://drive.google.com/file/d/1zABzvK_e7Txcah1FkyGnxAWEqCuR4bkQ/view?usp=sharing" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/file/d/1zABzvK_e7Txcah1FkyGnxAWEqCuR4bkQ/view?usp=sharing" -o yolor_p6.pt
 rm ./cookie
 
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" > /dev/null
